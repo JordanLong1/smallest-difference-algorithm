2955fe4cc6196934deda2aaeb7e83912306d36f4
Jordan <jordanleelong5@gmail.com>
2020-12-07 19:52:31 -0800

    finally solved with O(nlog(n) + mlog(m)) time and oconstant space

---
+++ newSteps.md
@@ -0,0 +1,17 @@
+sort both arrays in ascending order
+
+smallest = infinity; 
+
+left = 0 
+right = 0 
+
+if val of left pointer is less than val of right pointer? 
+
+smallest = Math.abs(arrayOne[left] - arrayTwo[right])
+left++
+
+if val of right pointer is greater than val of left pointer?
+smallest = Math.abs(arrayTwo[right] - arrayone[left])
+right++
+
+else case to make closest the closest val to 0..
--- smallestDiff.js
+++ smallestDiff.js
@@ -1,27 +1,36 @@
 function smallestDifference(arrayOne, arrayTwo) {
+  arrayOne = arrayOne.sort((a, b) => a-b ); 
+     arrayTwo = arrayTwo.sort((a, b) => a-b ); 
+   
+   
+     let left = 0; 
+   let right = 0;
+     let smallest = Infinity
+     let current = Infinity; 
+     let smallestPair = []
+   
+   
  
-    arrayOne = arrayOne.sort((a, b) => a-b ); 
-    arrayTwo = arrayTwo.sort((a, b) => a-b ); 
-
-    let smallest = Infinity; 
-		
-    let left = 0; 
-
-    let right = 0; 
-
-    while (left < arrayOne.length && right < arrayTwo.length) {
-    
-    if (arrayOne[left] < arrayTwo[right]) {
-		left++; 
-     smallest = [arrayOne[left], arrayTwo[right]]; 
-    
-    } else if (arrayOne[left] > arrayTwo[right]) {
-      right++; 
-    } 
-	  smallest = [arrayOne[left], arrayTwo[right]]; 
-		left++; 
-		
-    }
-    return smallest; 
-
-}
+     while (left< arrayOne.length && right < arrayTwo.length) {
+     let firstNum = arrayOne[left]; 
+       let secondNum = arrayTwo[right]; 
+       if (firstNum < secondNum) {
+         current = secondNum - firstNum; 
+         left++
+       } else if (secondNum < firstNum) {
+         current = firstNum - secondNum
+         right++
+       } else {
+         return [firstNum, secondNum]
+       }
+       
+       if (smallest > current) {
+         smallest = current; 
+         smallestPair = [firstNum, secondNum]
+       }
+       
+     }
+       
+     return smallestPair
+ 
+ }
